fcdb414afb0137b1da1af600023c0bb27b0dd91e
marcodlapaz <marcovalencia.dev@gmail.com>
2023-07-02 16:35:02 -0400

    Addgin operators folder

---
+++ 02_operators/01_arithmetic_operators.dart
@@ -0,0 +1,14 @@
+void main() {
+  int suma = 1 + 2;
+  int resta = 4 - 1;
+  int multiplicacion = 5 * 6;
+
+  double division = 10 / 3;
+  double modulo = 9 % 3;
+
+  print(suma);
+  print(resta);
+  print(multiplicacion);
+  print(division);
+  print(modulo);
+}
